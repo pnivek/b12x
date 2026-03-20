042cc2f2fd47c06be8bbeecd5ac55e274c1539a3
Luke Alonso <lalonso@gmail.com>
2026-03-20 04:01:56 -0700

    Revert "attn: transpose fp8 v packed stage words"
    This reverts commit 3fc19a565634a02b772a236bd65eec5bf3b14123.

--- b12x/attention/forward.py
+++ b12x/attention/forward.py
@@ -1069,65 +1069,19 @@ class SM120ForwardKernel:
         src_idx: Int32,
         stage_idx: Int32,
         tile_hdim_x: cutlass.Constexpr,
-        transpose_packed_words: cutlass.Constexpr = False,
     ):
         lane = cute.arch.lane_idx()
         del batch_idx
         mXu32 = cute.recast_tensor(mX, cutlass.Uint32)
         sXu32 = cute.recast_tensor(sX, cutlass.Uint32)
-        if const_expr(not transpose_packed_words):
-            words_per_row = tile_hdim_x // 4
-            total_packed = self.tile_n * words_per_row
-            for idx_iter in cutlass.range_constexpr(cute.ceil_div(total_packed, cute.arch.WARP_SIZE)):
-                packed_idx = lane + idx_iter * cute.arch.WARP_SIZE
-                if packed_idx < total_packed:
-                    row = packed_idx // words_per_row
-                    col_word = packed_idx - row * words_per_row
-                    sXu32[row, col_word, stage_idx] = mXu32[row, col_word, head_idx_kv, src_idx]
-            return
-
-        row_blocks = self.tile_n // 4
         words_per_row = tile_hdim_x // 4
-        total_tiles = row_blocks * words_per_row
-        shift8 = cutlass.Uint32(8)
-        shift16 = cutlass.Uint32(16)
-        shift24 = cutlass.Uint32(24)
-        mask = cutlass.Uint32(0xFF)
-        for idx_iter in cutlass.range_constexpr(cute.ceil_div(total_tiles, cute.arch.WARP_SIZE)):
-            tile_idx = lane + idx_iter * cute.arch.WARP_SIZE
-            if tile_idx < total_tiles:
-                row_block = tile_idx // words_per_row
-                col_word = tile_idx - row_block * words_per_row
-                row = row_block * 4
-                col = col_word * 4
-                packed0 = mXu32[row + 0, col_word, head_idx_kv, src_idx]
-                packed1 = mXu32[row + 1, col_word, head_idx_kv, src_idx]
-                packed2 = mXu32[row + 2, col_word, head_idx_kv, src_idx]
-                packed3 = mXu32[row + 3, col_word, head_idx_kv, src_idx]
-                sXu32[row_block, col + 0, stage_idx] = (
-                    (packed0 & mask)
-                    | ((packed1 & mask) << shift8)
-                    | ((packed2 & mask) << shift16)
-                    | ((packed3 & mask) << shift24)
-                )
-                sXu32[row_block, col + 1, stage_idx] = (
-                    ((packed0 >> shift8) & mask)
-                    | (((packed1 >> shift8) & mask) << shift8)
-                    | (((packed2 >> shift8) & mask) << shift16)
-                    | (((packed3 >> shift8) & mask) << shift24)
-                )
-                sXu32[row_block, col + 2, stage_idx] = (
-                    ((packed0 >> shift16) & mask)
-                    | (((packed1 >> shift16) & mask) << shift8)
-                    | (((packed2 >> shift16) & mask) << shift16)
-                    | (((packed3 >> shift16) & mask) << shift24)
-                )
-                sXu32[row_block, col + 3, stage_idx] = (
-                    ((packed0 >> shift24) & mask)
-                    | (((packed1 >> shift24) & mask) << shift8)
-                    | (((packed2 >> shift24) & mask) << shift16)
-                    | (((packed3 >> shift24) & mask) << shift24)
-                )
+        total_packed = self.tile_n * words_per_row
+        for idx_iter in cutlass.range_constexpr(cute.ceil_div(total_packed, cute.arch.WARP_SIZE)):
+            packed_idx = lane + idx_iter * cute.arch.WARP_SIZE
+            if packed_idx < total_packed:
+                row = packed_idx // words_per_row
+                col_word = packed_idx - row * words_per_row
+                sXu32[row, col_word, stage_idx] = mXu32[row, col_word, head_idx_kv, src_idx]
 
     @cute.jit
     def dequant_fp8_stage_shared(
@@ -1283,7 +1237,6 @@ class SM120ForwardKernel:
                         src_idx,
                         kv_producer_state.index,
                         self.tile_hdim,
-                        transpose_packed_words=False,
                     )
                     pipeline_k.producer_commit(kv_producer_state)
                 pipeline_v.producer_acquire(kv_producer_state)
@@ -1298,7 +1251,6 @@ class SM120ForwardKernel:
                         src_idx,
                         kv_producer_state.index,
                         self.tile_hdimv,
-                        transpose_packed_words=True,
                     )
                     pipeline_v.producer_commit(kv_producer_state)
                 kv_producer_state.advance()
